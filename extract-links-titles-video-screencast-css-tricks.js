7ae049390b8e1fa51ce9e238e41e667b792c5441
Giorgio Riccardi <griccardi@my.bcit.ca>
2016-08-16 06:02:46 -0700

    drafting an initial solution to sort what type of data we can retrieve

--- extract-links-titles-video-screencast-css-tricks.js
+++ extract-links-titles-video-screencast-css-tricks.js
@@ -13,5 +13,25 @@ console.log("Nav elements: ", videoScreencastLinks.length); // 4
 var videoScreencastLinks = document.getElementsByClassName('breadcrumbs video-nav breadcrumbs-top');
 console.log("Nav elements for Video Screencast: ", videoScreencastLinks.length); // 2
 
-// next steps:
-// checking what kind of data type we are outputtig
+// now that we determined that the array is just [0,1] we have to check that [0] === [1]
+var videoScreencastLinks = document.getElementsByClassName('breadcrumbs video-nav breadcrumbs-top');
+
+for ( i=0; i < videoScreencastLinks.length; i++) {
+console.log("Links for Video Screencast: ", videoScreencastLinks[i].innerHTML);
+}
+// if they are different they get listed separated, if not there will be a 2 on the top left console.log
+
+// but I want to compare if the content is the same
+var videoScreencastLinksTop = document.getElementsByClassName('breadcrumbs video-nav breadcrumbs-top')[0].innerHTML;
+var videoScreencastLinksBottom = document.getElementsByClassName('breadcrumbs video-nav breadcrumbs-top')[1].innerHTML;
+
+// before let's determine what they are: is it an object or a string?
+console.log(typeof(videoScreencastLinksTop));
+console.log(typeof(videoScreencastLinksBottom));
+
+if ( videoScreencastLinksTop === videoScreencastLinksBottom ) {
+	console.info('they are ===');
+} else {
+	console.error('they are !===');
+}
+// ok, they are equal, let's move on!
