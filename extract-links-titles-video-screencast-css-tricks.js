7e7e106d4c24a6a7735e223fa740ab81e7be6459
Giorgio Riccardi <griccardi@my.bcit.ca>
2016-08-22 19:36:41 -0700

    better comments, first draft of indexing the array of the links, it triggers an error with the array on the video-card group

--- extract-links-titles-video-screencast-css-tricks.js
+++ extract-links-titles-video-screencast-css-tricks.js
@@ -16,51 +16,74 @@ console.log("Nav elements for Video Screencast: ", videoScreencastLinks.length);
 // now that we determined that the array is just [0,1] we have to check that [0] === [1]
 var videoScreencastLinks = document.getElementsByClassName('breadcrumbs video-nav breadcrumbs-top');
 
-for ( i=0; i < videoScreencastLinks.length; i++) {
+for ( i=0; i < videoScreencastLinks.length; i++ ) {
 console.log("Links for Video Screencast: ", videoScreencastLinks[i].innerHTML);
 }
-// if they are different they get listed separated, if not there will be a 2 on the top left console.log
+// if they are different they get listed separated
 
 // but I want to compare if the content is the same
 var videoScreencastLinksTop = document.getElementsByClassName('breadcrumbs video-nav breadcrumbs-top')[0].innerHTML;
 var videoScreencastLinksBottom = document.getElementsByClassName('breadcrumbs video-nav breadcrumbs-top')[1].innerHTML;
 
-// before let's determine what they are: is it an object or a string?
+// before let's determine what they are: is it an object, a string or what?
 console.log(typeof(videoScreencastLinksTop));
 console.log(typeof(videoScreencastLinksBottom));
 
 if ( videoScreencastLinksTop === videoScreencastLinksBottom ) {
-	console.info('they are ===');
+	console.info('Ok, they are ===');
 } else {
-	console.error('they are !===');
+	alert('No, Links #top and #bottom are !===');
 }
 
 // Parsing the URLS
 // (this array will be used later when calling all pages via Ajax call)
 
-var urls = [];
+var pageUrls = [];
 
 for ( j=0; j < 10; j++ ) {
-	// urls += "https://css-tricks.com/video-screencasts/page/" + j + "/" + ", ";
-	urls.push("https://css-tricks.com/video-screencasts/page/" + j + "/");
+	// pageUrls += "https://css-tricks.com/video-screencasts/page/" + j + "/" + ", ";
+	pageUrls.push("https://css-tricks.com/video-screencasts/page/" + j + "/");
 }
 
-console.log(urls);
+//console.log(pageUrls);
 
-// Narrow down from the article content to the link
-
-var videoContainer = document.getElementsByClassName("video-card group")[0];
-var infoContainer = videoContainer.getElementsByClassName("video-info")[0];
-var allLinks = infoContainer.getElementsByTagName("a");
-
-for ( var g=0; g < allLinks.lenght; g++ ) {
-  var link = allLinks[g];
-  //do something next
+// method one:
+var index;
+for ( index = 0; index < pageUrls.length; index++ ) {
+    console.log(pageUrls[index]);
 }
 
-var text = allLinks[g].innerHTML; // just the title
-var link = allLinks[g]; // and the video link
+// method two:
+// pageUrls.forEach( function(listPages) {
+//     console.log(listPages);
+// });
 
-console.log(text + link);
+
+// Narrow down from the article content to the video link
+function singleVideoLink() {
+
+	var articles = document.getElementsByTagName("article");
+	for( var s = 0; s < articles.length; s++ ){
+	  console.log(articles[s]);
+	}
+	 
+	var videoContainer = document.getElementsByClassName("video-card group")[0]; // this array triggers an error! we either run the array on article or on the video-card group 
+	var infoContainer = videoContainer.getElementsByClassName("video-info")[0];
+	var videoLink = infoContainer.getElementsByTagName("a");
+
+	for ( var g = 0; g < videoLink.lenght; g++ ) {
+	  // console.log(videoLink[g]);
+	}
+
+	var text = videoLink[g].innerHTML; // just the title
+	var link = videoLink[g]; // and the video link
+
+	//console.log(text + link);
+	console.log(text);
+	console.log(link);
+	  
+}
+
+singleVideoLink();
 
 // Next, output all the liks in the array
